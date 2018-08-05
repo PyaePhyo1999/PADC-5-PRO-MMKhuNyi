8926f1872385034d961a27ad24de52dedfcbae4d
DESKTOP-S6KAUUS\Acer <ppm421999@gmail.com>
2018-08-05 16:26:13 +0630

    navigation

--- app/src/main/java/com/example/acer/kuunyi/activities/MainActivity.kt
+++ app/src/main/java/com/example/acer/kuunyi/activities/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.view.GravityCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -51,6 +52,11 @@ class MainActivity : BaseActivity() ,JobsListView {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        if (supportActionBar!=null){
+            supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp)
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        }
+
         mPresenter= ViewModelProviders.of(this).get(JobsListPresenter::class.java)
         mPresenter.initPresenter(this)
         mSmartScrollListener = SmartScrollListener(object :SmartScrollListener.SmartScrollListener{
@@ -86,10 +92,16 @@ class MainActivity : BaseActivity() ,JobsListView {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+        var id = item.itemId
+         if  (id==R.id.action_settings) {
+          return true
+         }
+            else if (id==android.R.id.home) {
+             drawLayout.openDrawer(GravityCompat.START)
+         }
+
+            return super.onOptionsItemSelected(item)
+
 
 
 
---
+++ app/src/main/java/com/example/acer/kuunyi/viewpods/UserAccountViewPod.kt
@@ -0,0 +1,20 @@
+package com.example.acer.kuunyi.viewpods
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+/**
+ * Created by Acer on 8/5/2018.
+ */
+class UserAccountViewPod: RelativeLayout {
+    constructor(context: Context) : super(context) {}
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
+
+}
+
+
+
